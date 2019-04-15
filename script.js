03e1213d64ccbfc8e5cb1c6e9a26dd3a7144c45e
Denys <47363351+Faceless22@users.noreply.github.com>
2019-04-15 21:55:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+// (1)
+function fib(n){
+var a = 1, b = 1;
+    for (var i = 3; i <= n; i++){
+        var c = a + b;
+         a = b, b = c;
+        }
+    return b;
+}
+console.log(fib(10));
+
+
+// (2)
+var n = 1000;
+var num = 0;
+while (n > 50){
+    num ++;
+    n /= 2;
+}
+console.log(n);
+console.log(num);
+
+
+
